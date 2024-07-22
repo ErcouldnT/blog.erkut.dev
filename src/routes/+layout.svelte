b90ee85aa74a328059d05892be7af5bf0204de7d
Ercode Techoglu <ercode@pm.me>
2024-07-23 01:36:15 +0300

    qqq

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,4 +39,29 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<slot />
+<div class="grid grid-rows-[auto_1fr_auto]">
+	<!-- Header -->
+	<header class="bg-red-500 p-4">(header)</header>
+	<!-- Page -->
+	<div class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_1fr)_200px]">
+		<!-- Sidebar (Left) -->
+		<!-- NOTE: hidden in smaller screen sizes -->
+		<aside class="sticky top-0 col-span-1 hidden h-screen bg-yellow-500 p-4 xl:block">
+			(sidebar)
+		</aside>
+		<!-- Main -->
+		<main class="col-span-1 space-y-4 bg-green-500 p-4">
+			<slot />
+			<!-- <p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p> -->
+		</main>
+		<!-- Sidebar (Right) -->
+		<!-- NOTE: hidden in smaller screen sizes -->
+		<aside class="sticky top-0 col-span-1 hidden h-screen bg-yellow-500 p-4 xl:block">
+			(sidebar)
+		</aside>
+	</div>
+	<!-- Footer -->
+	<footer class="bg-blue-500 p-4">(footer)</footer>
+</div>
