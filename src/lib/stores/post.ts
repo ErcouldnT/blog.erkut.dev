0fecc16ae98a03a32f74729540a5099a199ba2ea
Ercode Techoglu <ercode@pm.me>
2024-07-25 04:30:33 +0300

    fix: title regex

--- src/lib/stores/post.ts
+++ src/lib/stores/post.ts
@@ -2,4 +2,7 @@ import { persisted } from 'svelte-persisted-store';
 
 // First param `post` is the local storage key.
 // Second param is the initial value.
-export const post = persisted('post', '<h1>Başlık</h1>'); // new post!
+export const post = persisted(
+	'post',
+	'<h1>Başlık</h1><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p>'
+); // new post!
